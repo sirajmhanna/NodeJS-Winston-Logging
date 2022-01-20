b74bb936290452532cf4122504f198c393f21ab6
Siraj <sirajmhanna@hotmail.com>
2022-01-20 10:28:08 +0200

    Create winston.js

---
+++ config/winston.js
@@ -0,0 +1,26 @@
+const winston = require("winston");
+
+const loggerConfiguration = {
+  transports: [
+    new winston.transports.Console({
+      // or File
+      level: "warn",
+      filename: "logging/warn.log",
+      format: winston.format.combine(winston.format.json()),
+    }),
+    new winston.transports.Console({
+      // or File
+      level: "error",
+      filename: "logging/error.log",
+      format: winston.format.combine(winston.format.json()),
+    }),
+    new winston.transports.Console({
+      // or File
+      level: "info",
+      filename: "logging/all.log",
+      format: winston.format.combine(winston.format.json()),
+    }),
+  ],
+};
+
+module.exports = { winston, loggerConfiguration };
